4403d7deacb308a45eba0f0b87f1251ef00e2cd7
Dmitrii Dubskii <dudim55@gmail.com>
2025-07-15 17:22:56 +0300

    Enhance Playwright configuration by adding global test timeouts

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
     testDir: "./e2e/tests",
+    timeout: 30000, // глобальный таймаут для одного теста (30 сек)
+    expect: {
+        timeout: 15000, // таймаут для всех expect (15 сек)
+    },
     use: {
         baseURL: "http://localhost:6006",
         browserName: "chromium",

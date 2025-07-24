6c756d53979d838caf5129a956beb3b818ba2a63
Dmitrii Dubskii <dudim55@gmail.com>
2025-07-24 18:27:43 +0300

    text push

--- src/components/DesignTokens/index.ts
+++ src/components/DesignTokens/index.ts
@@ -2,5 +2,4 @@ import { DesignTokensCore } from "./DesignTokensCore";
 import { DesignTokensCoreThemeDark } from "./DesignTokensCoreThemeDark";
 import { DesignTokensComponents } from "./DesignTokensComponents";
 import { DesignTokensComponentsThemeDark } from "./DesignTokensComponentsThemeDark";
-
 export { DesignTokensCore, DesignTokensCoreThemeDark, DesignTokensComponents, DesignTokensComponentsThemeDark };

93e314f71800639dbb1283c64bcc8cb0e3c27dfd
Екатерина Табунщикова <ekaterinatabunshchikova@MacBook-Air-Ekaterina.local>
2025-09-12 15:50:12 +0300

    TRIPLEX-520 Доработана анимация, добавлен loader middle

--- src/components/Loader/LoaderMiddle/LoaderMiddle.tsx
+++ src/components/Loader/LoaderMiddle/LoaderMiddle.tsx
@@ -4,12 +4,13 @@ import clsx from "clsx";
 
 export const LoaderMiddle: React.FC = () => {
     return (
-        <span className={styles.loaderMiddle}>
+        <div className={styles.loaderMiddle}>
+            <span className={styles.line} />
             <span className={clsx(styles.dot, styles.dot1)} />
             <span className={clsx(styles.dot, styles.dot2)} />
             <span className={clsx(styles.dot, styles.dot3)} />
             <span className={clsx(styles.dot, styles.dot4)} />
-        </span>
+        </div>
     );
 };
 

d4ba661bbb12605c9b652605ffe576789415d9f7
Dmitrii Dubskii <dudim55@gmail.com>
2025-09-17 17:02:32 +0300

    TRIPLEX-578 DropdownList FC

--- src/components/Dropdown/desktop/DropdownList.tsx
+++ src/components/Dropdown/desktop/DropdownList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { DropdownListItem } from "./DropdownListItem";
 import { EVENT_KEY_CODES } from "@sberbusiness/triplex-next/utils/keyboard";
 import { DropdownListContext } from "@sberbusiness/triplex-next/components/Dropdown/DropdownListContext";
@@ -13,232 +13,169 @@ export interface IDropdownListProps extends React.HTMLAttributes<HTMLDivElement>
     listRef?: React.RefObject<HTMLDivElement>;
 }
 
-/** Состояние компонента DropdownList. */
-interface IDropdownListState {
-    /** Индекс текущего выделенного элемента списка при навигации с клавиатуры. */
-    activeListItemIndex?: number;
-}
+/** Индекс текущего выделенного элемента списка при навигации с клавиатуры. */
+type TActiveListItemIndex = number | undefined;
 
 /**
  * Компонент DropdownList.
  * Используется для обрамления вложенного списка и добавляет спику возможность навигации с клавиатуры.
  * В качестве children принимает только DropdownList.Item.
  */
-export class DropdownList extends React.Component<IDropdownListProps, IDropdownListState> {
-    public static Item = DropdownListItem;
+export interface IDropdownListComponent extends React.FC<IDropdownListProps> {
+    Item: typeof DropdownListItem;
+}
 
-    static contextType = DropdownListContext;
+export const DropdownList: IDropdownListComponent = (props) => {
+    const { children, className, dropdownOpened, listRef, ...htmlDivAttributes } = props;
+    const classNames = clsx(stylesDropdownList.dropdownList, className);
 
-    declare context: React.ContextType<typeof DropdownListContext>;
+    const { activeDescendant, setActiveDescendant } = useContext(DropdownListContext);
 
-    // Массив рефов на элементы списка.
-    private listItems: React.RefObject<HTMLDivElement>[] = [];
     // Ref контейнера списка.
-    private containerRef: React.RefObject<HTMLDivElement>;
+    const containerRef = listRef || React.createRef<HTMLDivElement>();
+    // Массив рефов на элементы списка.
+    const listItemsRef = useRef<Array<React.RefObject<HTMLDivElement>>>([]);
+    const [activeListItemIndex, setActiveListItemIndex] = useState<TActiveListItemIndex>(undefined);
 
-    constructor(props: IDropdownListProps) {
-        super(props);
+    const childrenCount = React.Children.count(children);
 
-        this.containerRef = props.listRef || React.createRef<HTMLDivElement>();
-    }
-
-    state: IDropdownListState = {
-        activeListItemIndex: undefined,
-    };
-
-    public componentDidMount(): void {
-        const { dropdownOpened } = this.props;
-
-        if (dropdownOpened) {
-            document.addEventListener("keydown", this.handleKeyDown);
-
-            this.handleOpenDropdown();
-        }
-    }
-
-    public componentDidUpdate(prevProps: IDropdownListProps, prevState: IDropdownListState): void {
-        const { dropdownOpened } = this.props;
-        const { activeListItemIndex } = this.state;
-        const { activeDescendant, setActiveDescendant } = this.context;
-        const { dropdownOpened: prevDropdownOpened } = prevProps;
-        const { activeListItemIndex: prevActiveListItemIndex } = prevState;
-
-        if (dropdownOpened && !prevDropdownOpened) {
-            document.addEventListener("keydown", this.handleKeyDown);
-
-            this.handleOpenDropdown();
-        } else if (!dropdownOpened && prevDropdownOpened) {
-            document.removeEventListener("keydown", this.handleKeyDown);
-            if (activeDescendant !== undefined) {
-                setActiveDescendant();
-            }
-        } else if (activeListItemIndex !== undefined && prevActiveListItemIndex !== activeListItemIndex) {
-            setActiveDescendant(this.listItems[activeListItemIndex]?.current?.id);
-        }
-    }
-
-    public componentWillUnmount(): void {
-        const { activeDescendant, setActiveDescendant } = this.context;
-
-        document.removeEventListener("keydown", this.handleKeyDown);
-        if (activeDescendant !== undefined) {
-            setActiveDescendant();
-        }
-    }
-
-    public render(): JSX.Element {
-        const { children, className, dropdownOpened, listRef, ...htmlDivAttributes } = this.props;
-        const classNames = clsx(stylesDropdownList.dropdownList, className);
-
-        return (
-            <div className={classNames} role="listbox" ref={this.containerRef} {...htmlDivAttributes}>
-                {this.renderChildren()}
-            </div>
-        );
-    }
-
-    private setListItemRef: (index: number) => React.RefObject<HTMLDivElement> = (index) => {
+    const setListItemRef = (index: number): React.RefObject<HTMLDivElement> => {
         const ref = React.createRef<HTMLDivElement>();
-        this.listItems[index] = ref;
+        listItemsRef.current[index] = ref;
         return ref;
     };
 
-    /**
-     * Обработка открытия Dropdown.
-     * Скролл к выбранному значению и установка activeListItemIndex.
-     */
-    private handleOpenDropdown = () => {
-        const { children } = this.props;
-
-        // Есть выбранное значение.
-        let isSelectedItemExist = false;
-
-        React.Children.forEach(children, (child, index) => {
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-            if (child && child.props && child.props.selected) {
-                isSelectedItemExist = true;
-                this.scrollContainerToItem(index);
-
-                this.setState({
-                    activeListItemIndex: index,
-                });
-            }
-        });
-
-        // Нет выбранного значения, контейнер скроллится к первому значению.
-        if (!isSelectedItemExist) {
-            // activeListItemIndex - первый элемент списка.
-            this.setState({
-                activeListItemIndex: 0,
-            });
-            this.scrollContainerToTop();
-        }
-    };
-
-    /**
-     * Скролл контейнера к элементу списка.
-     */
-    private scrollContainerToItem = (itemIndex: number) => {
-        const parent = this.containerRef?.current;
-        const activeItem = this.listItems[itemIndex]?.current;
+    const scrollContainerToItem = (itemIndex: number) => {
+        const parent = containerRef?.current;
+        const activeItem = listItemsRef.current[itemIndex]?.current;
 
         if (parent && activeItem) {
             const { top: parentTop, bottom: parentBottom } = parent.getBoundingClientRect();
             const { top: itemTop, bottom: itemBottom } = activeItem.getBoundingClientRect();
             const offset = 4;
 
-            // Item выше верхней границы parent.
             if (parentTop > itemTop) {
                 parent.scrollTop = parent.scrollTop - parentTop + itemTop - offset;
             } else if (itemBottom > parentBottom) {
-                // Item ниже нижней границы parent.
                 parent.scrollTop = parent.scrollTop + itemBottom - parentBottom + offset;
             }
         }
     };
 
-    /**
-     * Скролл контейнера к началу списка.
-     */
-    private scrollContainerToTop = () => {
-        const container = this.containerRef?.current;
-
+    const scrollContainerToTop = () => {
+        const container = containerRef?.current;
         if (container) {
             container.scrollTop = 0;
         }
     };
 
-    private handleKeyDown = (event: KeyboardEvent) => {
-        const { keyCode } = event;
-        const { children } = this.props;
-        const { activeListItemIndex } = this.state;
-        const childrenLength = React.Children.count(children);
-        let nextActiveListItemIndex;
+    // Подписка на keydown когда открыт, с актуальным индексом.
+    useEffect(() => {
+        if (!dropdownOpened) {
+            return;
+        }
 
-        if (keyCode === EVENT_KEY_CODES.ARROW_DOWN) {
-            if (activeListItemIndex !== undefined) {
-                if (activeListItemIndex < childrenLength - 1) {
-                    // Следующий за текущим элемент списка.
-                    nextActiveListItemIndex = activeListItemIndex + 1;
+        const handleKeyDown = (event: KeyboardEvent) => {
+            const { keyCode } = event;
+            const childrenLength = childrenCount;
+            const currentIndex = activeListItemIndex;
+            let nextActiveListItemIndex: number | undefined;
+
+            if (keyCode === EVENT_KEY_CODES.ARROW_DOWN) {
+                if (currentIndex !== undefined) {
+                    if (currentIndex < childrenLength - 1) {
+                        nextActiveListItemIndex = currentIndex + 1;
+                    } else {
+                        nextActiveListItemIndex = 0;
+                    }
                 } else {
-                    // Первый элемент списка.
                     nextActiveListItemIndex = 0;
                 }
-            } else {
-                // Первый элемент списка.
-                nextActiveListItemIndex = 0;
-            }
-
-            event.preventDefault();
-        } else if (keyCode === EVENT_KEY_CODES.ARROW_UP) {
-            if (activeListItemIndex !== undefined) {
-                if (activeListItemIndex > 0) {
-                    // Предыдущий элемент списка.
-                    nextActiveListItemIndex = activeListItemIndex - 1;
+                event.preventDefault();
+            } else if (keyCode === EVENT_KEY_CODES.ARROW_UP) {
+                if (currentIndex !== undefined) {
+                    if (currentIndex > 0) {
+                        nextActiveListItemIndex = currentIndex - 1;
+                    } else {
+                        nextActiveListItemIndex = childrenLength - 1;
+                    }
                 } else {
-                    // Последний элемент списка.
                     nextActiveListItemIndex = childrenLength - 1;
                 }
-            } else {
-                // Последний элемент списка.
-                nextActiveListItemIndex = childrenLength - 1;
+                event.preventDefault();
             }
 
-            event.preventDefault();
-        }
-
-        if (nextActiveListItemIndex !== undefined && activeListItemIndex !== nextActiveListItemIndex) {
-            this.scrollContainerToItem(nextActiveListItemIndex);
-
-            this.setState({
-                activeListItemIndex: nextActiveListItemIndex,
-            });
-        }
-    };
-
-    private renderChildren = () => {
-        const { children, dropdownOpened } = this.props;
-        const { activeListItemIndex } = this.state;
-
-        return React.Children.map(children, (child, index) => {
-            if (!child) {
-                return;
+            if (nextActiveListItemIndex !== undefined && currentIndex !== nextActiveListItemIndex) {
+                scrollContainerToItem(nextActiveListItemIndex);
+                setActiveListItemIndex(nextActiveListItemIndex);
             }
+        };
 
-            return React.cloneElement(child as React.ReactElement, {
-                active: dropdownOpened && activeListItemIndex === index,
-                onMouseOver: (event: MouseEvent) => {
-                    this.setState({
-                        activeListItemIndex: index,
-                    });
+        document.addEventListener("keydown", handleKeyDown);
+        return () => {
+            document.removeEventListener("keydown", handleKeyDown);
+        };
+    }, [dropdownOpened, activeListItemIndex, childrenCount]);
 
-                    // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
-                    (child as React.ReactElement).props.onMouseOver?.(event);
-                },
-                ref: this.setListItemRef(index),
-            });
+    // Установка активного элемента и скролла при открытии.
+    useEffect(() => {
+        if (!dropdownOpened) {
+            return;
+        }
+        let isSelectedItemExist = false;
+        React.Children.forEach(children, (child, index) => {
+            // @ts-ignore
+            if (child && child.props && child.props.selected) {
+                isSelectedItemExist = true;
+                scrollContainerToItem(index);
+                setActiveListItemIndex(index);
+            }
         });
-    };
-}
+        if (!isSelectedItemExist) {
+            setActiveListItemIndex(0);
+            scrollContainerToTop();
+        }
+    }, [dropdownOpened]);
+
+    // Синхронизация activeDescendant при изменении activeListItemIndex
+    useEffect(() => {
+        if (dropdownOpened && activeListItemIndex !== undefined) {
+            setActiveDescendant(listItemsRef.current[activeListItemIndex]?.current?.id);
+            return;
+        }
+        if (!dropdownOpened && activeDescendant !== undefined) {
+            setActiveDescendant();
+        }
+    }, [dropdownOpened, activeListItemIndex, activeDescendant, setActiveDescendant]);
+
+    useEffect(() => {
+        return () => {
+            if (activeDescendant !== undefined) {
+                setActiveDescendant();
+            }
+        };
+    }, [activeDescendant, setActiveDescendant]);
+
+    const renderedChildren = React.Children.map(children, (child, index) => {
+        if (!child) {
+            return;
+        }
+
+        return React.cloneElement(child as React.ReactElement, {
+            active: dropdownOpened && activeListItemIndex === index,
+            onMouseOver: (event: React.MouseEvent) => {
+                setActiveListItemIndex(index);
+                (child as React.ReactElement).props.onMouseOver?.(event);
+            },
+            ref: setListItemRef(index),
+        });
+    });
+
+    return (
+        <div className={classNames} role="listbox" ref={containerRef} {...htmlDivAttributes}>
+            {renderedChildren}
+        </div>
+    );
+};
+
+DropdownList.Item = DropdownListItem;

f26bcea638c74c71868a3cdcd7d34dc919d31017
pidtchay <pidtchay@gmail.com>
2025-10-03 16:01:15 +0300

    TRIPLEX-594 Card Static/Action: Исправлены расхождения между макетами и реализацией

--- src/components/DesignTokens/components/Card.ts
+++ src/components/DesignTokens/components/Card.ts
@@ -46,13 +46,13 @@ export const Card_Tokens: TDesignTokensComponentsCardValues = {
     Action_General_Background_Selected: [{ ref: "ColorNeutral.90" }, { ref: "ColorDarkNeutral.80" }], // var(--triplex-next-Card-Action_General_Background_Selected)
     Action_General_Background_Selected_Hover: [{ ref: "ColorNeutral.70" }, { ref: "ColorDarkNeutral.80" }], // var(--triplex-next-Card-Action_General_Background_Selected_Hover)
 
-    Action_Secondary_Background: [{ ref: "ColorNeutral.90" }, { ref: "ColorDarkNeutral.80" }], // var(--triplex-next-Card-Action_Secondary_Background)
+    Action_Secondary_Background: [{ ref: "ColorNeutral.90" }, { ref: "ColorDarkNeutral.50" }], // var(--triplex-next-Card-Action_Secondary_Background)
     Action_Secondary_Background_Hover: [{ ref: "ColorNeutral.60" }, { ref: "ColorDarkNeutral.60" }], // var(--triplex-next-Card-Action_Secondary_Background_Hover)
-    Action_Secondary_Background_Selected: [{ ref: "ColorNeutral.40" }, { ref: "ColorDarkNeutral.100" }], // var(--triplex-next-Card-Action_Secondary_Background_Selected)
+    Action_Secondary_Background_Selected: [{ ref: "ColorNeutral.40" }, { ref: "ColorDarkNeutral.70" }], // var(--triplex-next-Card-Action_Secondary_Background_Selected)
     Action_Secondary_Background_Selected_Hover: [{ ref: "ColorNeutral.40" }, { ref: "ColorDarkNeutral.70" }], // var(--triplex-next-Card-Action_Secondary_Background_Selected_Hover)
 
     // Common
     BorderColor_Focus: [{ ref: "ColorWarning.80" }, { ref: "ColorWarning.80" }], // var(--triplex-next-Card-BorderColor_Focus)
-    Shadow_Default: [{ value: "0 0 7px 0 rgba(31, 31, 34, 0.25)" }, { value: "none" }], // var(--triplex-next-Card-Shadow_Default)
-    Shadow_Hover: [{ value: "0 0 14px 0 rgba(31, 31, 34, 0.25)" }, { value: "none" }], // var(--triplex-next-Card-Shadow_Hover)
+    Shadow_Default: [{ value: "0 0 7px 0 rgba(31, 31, 34, 0.25)" }, { value: "0 0 7px 0 rgba(0, 0, 0, 0.35)" }], // var(--triplex-next-Card-Shadow_Default)
+    Shadow_Hover: [{ value: "0 0 14px 0 rgba(31, 31, 34, 0.25)" }, { value: "0 0 14px 0 rgba(0, 0, 0, 0.35)" }], // var(--triplex-next-Card-Shadow_Hover)
 };

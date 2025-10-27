b5dd8bd6d3742b5d5d2804538476dfa63b97d021
Дубский Дмитрий Игоревич <dubskiy.d.i@sberbank.ru>
2025-10-27 14:37:53 +0300

    TRIPLEX-631 Chip initial

---
+++ src/components/Chip/index.ts
@@ -0,0 +1,6 @@
+export * from "./Chip";
+export * from "./ChipClearButton";
+export * from "./ChipDropdownArrow";
+export * from "./ChipIcon";
+export * from "./ChipOptions";
+export * from "./ChipSort";
---
+++ src/components/ChipGroup/ChipGroup.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { classnames } from "@sber-business/triplex/utils/classnames/classnames";
+
+export interface IChipGroupProps extends React.HTMLAttributes<HTMLDivElement> {
+    /** Чипсы выводятся в одну строку со скроллом. */
+    oneLine?: boolean;
+}
+
+/**
+ * Компонент Chips.
+ * Контейнер компонентов Chip.
+ */
+export const ChipGroup = React.forwardRef<HTMLDivElement, IChipGroupProps>(
+    ({ children, className, oneLine, ...rest }, ref) => (
+        <div
+            className={classnames("cssClass[chipGroup]", className, {
+                "cssClass[multiLine]": !oneLine,
+                "cssClass[oneLine]": Boolean(oneLine),
+            })}
+            {...rest}
+            ref={ref}
+            data-tx={process.env.npm_package_version}
+        >
+            {children}
+        </div>
+    ),
+);
+
+ChipGroup.displayName = "ChipGroup";

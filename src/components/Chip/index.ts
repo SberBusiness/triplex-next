b6d2688d73dfc90c759ec947dc103f0864e9d189
Екатерина Табунщикова <ekaterinatabunshchikova@gmail.com>
2025-12-03 14:02:15 +0300

    Добавлен компонент

--- src/components/Chip/index.ts
+++ src/components/Chip/index.ts
@@ -4,4 +4,5 @@ export * from "./ChipDropdownArrow";
 export * from "./ChipIcon";
 export * from "./ChipOptions";
 export * from "./ChipSelect";
+export * from "./ChipSort";
 export * from "./ChipSuggest";

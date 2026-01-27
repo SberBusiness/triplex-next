69ea93f74f93812322daf2ffc6b144c2374db6a7
Екатерина Табунщикова <ekaterinatabunshchikova@gmail.com>
2026-01-27 09:39:41 +0300

    Добавлены рамеры SM, LG для CheckboxTree и CheckboxTreeExtended, добавлены тесты для CheckboxTree

--- src/components/CheckboxTreeExtended/components/CheckboxTreeExtendedCheckbox.tsx
+++ src/components/CheckboxTreeExtended/components/CheckboxTreeExtendedCheckbox.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import { Checkbox, ICheckboxProps } from "../../Checkbox/Checkbox";
 import clsx from "clsx";
 import { isStaticCheckboxTreeExtended } from "../isStaticCheckboxTreeExtended";
 import styles from "../styles/CheckboxTreeExtended.module.less";
+import { CheckboxTreeExtendedContext } from "../CheckboxTreeExtendedContext";
 
 /**
  * Свойства CheckboxTreeExtendedCheckbox.
@@ -18,42 +19,42 @@ interface ICheckboxTreeExtendedCheckboxProps extends ICheckboxProps {
  * Обертка над базовым компонентом чекбокс.
  * Используется для фокуса чекбокса при перемещении с клавиатуры.
  */
-export class CheckboxTreeExtendedCheckbox extends React.Component<ICheckboxTreeExtendedCheckboxProps> {
-    private checkboxNode?: HTMLInputElement;
+export const CheckboxTreeExtendedCheckbox: React.FC<ICheckboxTreeExtendedCheckboxProps> = ({
+    active,
+    className,
+    opened,
+    ...checkboxProps
+}) => {
+    const checkboxNode = useRef<HTMLInputElement | null>(null);
+    const { size } = useContext(CheckboxTreeExtendedContext);
 
-    public componentDidUpdate(prevProps: ICheckboxTreeExtendedCheckboxProps): void {
-        const { active } = this.props;
-        const { active: prevActive } = prevProps;
-
-        // Триггер фокуса на чекбоксе при изменении флага активности при перемещении по дереву с клавиатуры. Если нода имеет дочерние ноды, то фокус получает не чекбокс, а CheckboxTreeExtendedArrow.
-        if (active && !prevActive) {
-            // При взаимодействии мышью триггер фокуса не нужен.
-            if (!document.activeElement?.contains(this.checkboxNode as Node)) {
-                this.checkboxNode?.focus();
-            }
+    // Триггер фокуса на чекбоксе при изменении флага активности при перемещении по дереву с клавиатуры. Если нода имеет дочерние ноды, то фокус получает не чекбокс, а CheckboxTreeExtendedArrow.
+    useEffect(() => {
+        // При взаимодействии мышью триггер фокуса не нужен.
+        if (!document.activeElement?.contains(checkboxNode.current as Node)) {
+            checkboxNode.current?.focus();
         }
-    }
+    }, [active]);
 
-    public render(): JSX.Element {
-        const { active, className, opened, ...checkboxProps } = this.props;
-
-        return (
-            <Checkbox
-                className={clsx(styles.checkboxTreeCheckbox, className)}
-                ref={this.setCheckboxNode}
-                labelAttributes={{ className: styles.checkboxTreeCheckboxLabel, onFocus: this.handleFocus }}
-                {...checkboxProps}
-            />
-        );
-    }
-
-    private handleFocus = (event: React.FocusEvent<HTMLLabelElement>) => {
+    const handleFocus = (event: React.FocusEvent<HTMLLabelElement>) => {
         // Предотвращает всплытие до ноды дерева.
         // eslint-disable-next-line @typescript-eslint/no-unused-expressions
         !isStaticCheckboxTreeExtended && event.stopPropagation();
     };
 
-    private setCheckboxNode = (DOMNode: HTMLInputElement) => {
-        this.checkboxNode = DOMNode;
+    const setCheckboxNode = (DOMNode: HTMLInputElement) => {
+        checkboxNode.current = DOMNode;
     };
-}
+
+    return (
+        <Checkbox
+            className={clsx(styles.checkboxTreeCheckbox, className)}
+            ref={setCheckboxNode}
+            labelAttributes={{ className: styles.checkboxTreeCheckboxLabel, onFocus: handleFocus }}
+            size={size}
+            {...checkboxProps}
+        />
+    );
+};
+
+CheckboxTreeExtendedCheckbox.displayName = "CheckboxTreeExtendedCheckbox";

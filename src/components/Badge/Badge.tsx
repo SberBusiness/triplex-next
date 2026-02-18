daa1603d01017c63890f705c356c14e9810dd8e7
Dmitrii Dubskii <dudim55@gmail.com>
2026-02-18 19:06:32 +0300

    TRIPLEX-778 Update Badge

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -2,18 +2,34 @@ import React from "react";
 import clsx from "clsx";
 import { IBadgeProps } from "./types";
 import { createSizeToClassNameMap } from "../../utils/classNameMaps";
+import { BadgeDot } from "./BadgeDot";
+import { BadgeContent } from "./components/BadgeContent";
+import { BadgePrefix } from "./components/BadgePrefix";
+import { BadgePostfix } from "./components/BadgePostfix";
 import styles from "./styles/Badge.module.less";
 
-// Соответствие размера имени класса.
 const SIZE_TO_CLASS_NAME_MAP = createSizeToClassNameMap(styles);
 
 /** Индикатор статуса или уведомления. */
-export const Badge = React.forwardRef<HTMLSpanElement, IBadgeProps>(
-    ({ children, className, size, ...restProps }, ref) => (
-        <span className={clsx(styles.badge, SIZE_TO_CLASS_NAME_MAP[size], className)} {...restProps} ref={ref}>
-            {children}
-        </span>
+export const Badge = Object.assign(
+    React.forwardRef<HTMLSpanElement, IBadgeProps>(
+        ({ children, className, size, prefix, postfix, ...restProps }, ref) => (
+            <span className={clsx(styles.badge, SIZE_TO_CLASS_NAME_MAP[size], className)} {...restProps} ref={ref}>
+                {prefix ? <BadgePrefix>{prefix}</BadgePrefix> : null}
+
+                {children ? (
+                    <BadgeContent size={size} noPaddingLeft={Boolean(prefix)} noPaddingRight={Boolean(postfix)}>
+                        {children}
+                    </BadgeContent>
+                ) : null}
+
+                {postfix ? <BadgePostfix>{postfix}</BadgePostfix> : null}
+            </span>
+        ),
     ),
+    {
+        Dot: BadgeDot,
+    },
 );
 
 Badge.displayName = "Badge";

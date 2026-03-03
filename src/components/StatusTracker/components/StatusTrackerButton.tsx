c254e29a27fb29e9589c39461f9cc1fe3ee08a4a
Dmitrii Dubskii <dudim55@gmail.com>
2026-03-03 23:51:56 +0300

    TRIPLEX-802 StatusTrackerButton text align

--- src/components/StatusTracker/components/StatusTrackerButton.tsx
+++ src/components/StatusTracker/components/StatusTrackerButton.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import clsx from "clsx";
 import styles from "@sberbusiness/triplex-next/components/StatusTracker/styles/StatusTracker.module.less";
-import { Button, TButtonProps } from "@sberbusiness/triplex-next/components/Button";
+import { Button, IButtonLinkProps, TButtonProps } from "@sberbusiness/triplex-next/components/Button";
 
 /** Компонент кнопки для футера статус-трекера. */
-export const StatusTrackerButton: React.FC<TButtonProps> = (props) => {
-    const { children, className, ...rest } = props;
-    return (
-        <Button className={clsx(styles.statusTrackerButton, className)} {...rest}>
-            {children}
-        </Button>
-    );
-};
+export const StatusTrackerButton: React.FC<Exclude<TButtonProps, IButtonLinkProps>> = ({
+    children,
+    className,
+    ...rest
+}) => (
+    <Button className={clsx(styles.statusTrackerButton, className)} block {...rest}>
+        {children}
+    </Button>
+);

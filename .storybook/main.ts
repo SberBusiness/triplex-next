c4feee2aff637410166ca504315f169a220f4d97
Dmitrii Dubskii <dudim55@gmail.com>
2026-03-27 17:15:55 +0300

    TRIPLEX-820 Update Storybook configuration: replace Skeleton story with MarkerStatus story and remove MarkerStatus story file to streamline component documentation.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,7 +11,7 @@ const config: StorybookConfig = {
     },
     // stories: ["../stories/**/*.stories.@(ts|tsx|mdx)", "../stories/**/*.mdx"],
     // для отладки конкретного компонента
-    stories: ["../stories/Skeleton/Skeleton.stories.tsx"],
+    stories: ["../stories/MarkerStatus/MarkerStatus.stories.tsx"],
     typescript: {
         reactDocgen: "react-docgen-typescript",
         reactDocgenTypescriptOptions: {

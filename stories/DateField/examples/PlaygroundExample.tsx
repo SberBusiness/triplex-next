6d24b87dae6d58467e171a0b2add41dc53383897
aromny-w <55492276+aromny-w@users.noreply.github.com>
2026-04-28 15:40:54 +0300

    TRIPLEX-721 DateField | Замечания

--- stories/DateField/examples/PlaygroundExample.tsx
+++ stories/DateField/examples/PlaygroundExample.tsx
@@ -1,11 +1,56 @@
-import React, { useState } from "react";
-import { DateField } from "@sberbusiness/triplex-next";
+import React, { useState, useCallback } from "react";
+import {
+    DateField,
+    HelpBox,
+    Text,
+    Link,
+    ETooltipSize,
+    ETextSize,
+    EFormFieldStatus,
+    EFontType,
+} from "@sberbusiness/triplex-next";
+import type { PlaygroundArgs } from "../DateField.stories";
 
-export const PlaygroundExample = (args: React.ComponentProps<typeof DateField>) => {
+const STATUS_TO_DESCRIPTION_FONT_TYPE_MAP: Record<EFormFieldStatus, EFontType> = {
+    [EFormFieldStatus.DEFAULT]: EFontType.SECONDARY,
+    [EFormFieldStatus.DISABLED]: EFontType.SECONDARY,
+    [EFormFieldStatus.ERROR]: EFontType.ERROR,
+    [EFormFieldStatus.WARNING]: EFontType.WARNING,
+};
+
+export const PlaygroundExample = ({
+    status = EFormFieldStatus.DEFAULT,
+    withPostfix,
+    withDescription,
+    ...restArgs
+}: PlaygroundArgs) => {
     const [value, setValue] = useState("");
+
+    const handleLinkClick = useCallback<React.MouseEventHandler<HTMLAnchorElement>>((event) => {
+        event.preventDefault();
+    }, []);
+
     return (
-        <div style={{ maxWidth: "300px" }}>
-            <DateField {...args} value={value} onChange={setValue} />
+        <div style={{ maxWidth: 300 }}>
+            <DateField
+                {...restArgs}
+                value={value}
+                status={status}
+                onChange={setValue}
+                targetProps={{
+                    postfix: withPostfix && (
+                        <HelpBox tooltipSize={ETooltipSize.SM}>Helpful details appear here</HelpBox>
+                    ),
+                    description: withDescription && (
+                        <Text size={ETextSize.B4} type={STATUS_TO_DESCRIPTION_FONT_TYPE_MAP[status]}>
+                            (21) Description{" "}
+                            <Link href="#" onClick={handleLinkClick}>
+                                Link text
+                            </Link>
+                        </Text>
+                    ),
+                }}
+            />
         </div>
     );
 };

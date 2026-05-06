0e5a567b79f607bd76cb43ece11663f269d14c38
Dmitrii Dubskii <dudim55@gmail.com>
2026-05-06 17:28:07 +0300

    TRIPLEX-778 Badge: добавить width: fit-content от растягивания во flex
    Cross-axis stretch во flex-колонке тянул inline-flex Badge на полную
    ширину. width: fit-content фиксирует поведение «по контенту» и снимает
    необходимость в alignItems: flex-start у потребителей.
    
    Из SizesExample убран соответствующий костыль alignItems.

--- stories/Badge/examples/SizesExample.tsx
+++ stories/Badge/examples/SizesExample.tsx
@@ -13,7 +13,7 @@ import {
 
 export const SizesExample = () => (
     <div style={{ display: "flex", flexDirection: "column", gap: "16px", maxWidth: "150px" }}>
-        <div style={{ display: "flex", flexDirection: "column", gap: "8px", alignItems: "flex-start" }}>
+        <div style={{ display: "flex", flexDirection: "column", gap: "8px" }}>
             <div style={{ marginBottom: "8px", fontSize: "16px", fontWeight: "700" }}>SM</div>
             <Badge size={EComponentSize.SM}>
                 <Caption size={ECaptionSize.C1} weight={EFontWeightCaption.REGULAR} type={EFontType.PRIMARY_INVERT}>
@@ -21,7 +21,7 @@ export const SizesExample = () => (
                 </Caption>
             </Badge>
         </div>
-        <div style={{ display: "flex", flexDirection: "column", gap: "8px", alignItems: "flex-start" }}>
+        <div style={{ display: "flex", flexDirection: "column", gap: "8px" }}>
             <div style={{ marginBottom: "8px", fontSize: "16px", fontWeight: "700" }}>MD</div>
             <Badge size={EComponentSize.MD}>
                 <Text size={ETextSize.B4} weight={EFontWeightText.REGULAR} type={EFontType.PRIMARY_INVERT}>
@@ -29,7 +29,7 @@ export const SizesExample = () => (
                 </Text>
             </Badge>
         </div>
-        <div style={{ display: "flex", flexDirection: "column", gap: "8px", alignItems: "flex-start" }}>
+        <div style={{ display: "flex", flexDirection: "column", gap: "8px" }}>
             <div style={{ marginBottom: "8px", fontSize: "16px", fontWeight: "700" }}>LG</div>
             <Badge size={EComponentSize.LG}>
                 <Text size={ETextSize.B3} weight={EFontWeightText.REGULAR} type={EFontType.PRIMARY_INVERT}>

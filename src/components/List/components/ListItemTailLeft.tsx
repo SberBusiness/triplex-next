56a69e6f1561910ab520402c2bad87d3cafb2cbc
Dmitrii Dubskii <dudim55@gmail.com>
2026-06-17 16:27:25 +0300

    TRIPLEX-852 List: forwardRef указывает на HTMLSpanElement

--- src/components/List/components/ListItemTailLeft.tsx
+++ src/components/List/components/ListItemTailLeft.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import clsx from "clsx";
 import styles from "../styles/ListItemTail.module.less";
 
-export interface IListItemTailLeftProps extends React.HTMLAttributes<HTMLDivElement> {
+export interface IListItemTailLeftProps extends React.HTMLAttributes<HTMLSpanElement> {
     children?: never;
 }
 
 /** Хвост listItem, видимый при свайпе вправо. Размещается внутри компонента SwipeableArea. */
-export const ListItemTailLeft = React.forwardRef<HTMLDivElement, IListItemTailLeftProps>(
+export const ListItemTailLeft = React.forwardRef<HTMLSpanElement, IListItemTailLeftProps>(
     ({ className, ...rest }, ref) => (
         <span className={clsx(styles.listItemTail, styles.listItemTailLeft, className)} ref={ref} {...rest}>
             <span className={styles.listItemTailLine} />

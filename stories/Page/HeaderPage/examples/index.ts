06af17d885c1da9e877256ebde85842c927e413f
Екатерина Табунщикова <ekaterinatabunshchikova@gmail.com>
2026-07-07 15:55:04 +0300

    TRIPLEX-770 HeaderPage Исправлено расположение HeaderPageTabs

--- stories/Page/HeaderPage/examples/index.ts
+++ stories/Page/HeaderPage/examples/index.ts
@@ -3,3 +3,5 @@ export * from "./Default";
 export { default as DefaultSource } from "./Default?raw";
 export * from "./Types";
 export { default as TypesSource } from "./Types?raw";
+export * from "./ExampleWithTabs";
+export { default as ExampleWithTabsSource } from "./ExampleWithTabs?raw";

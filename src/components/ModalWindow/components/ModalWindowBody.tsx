f2cd69b6b8ac56f17cf5390d7fc3e1f6e94cf6d1
Екатерина Табунщикова <ekaterinatabunshchikova@gmail.com>
2026-08-06 16:19:39 +0300

    TRIPLEX-953 Island Добавлено состояние loading

--- src/components/ModalWindow/components/ModalWindowBody.tsx
+++ src/components/ModalWindow/components/ModalWindowBody.tsx
@@ -5,7 +5,13 @@ import { IIslandProps, Island } from "../../Island/Island";
 import { EComponentSize } from "../../../enums/EComponentSize";
 import { EIslandType } from "../../Island/enums";
 
-export interface IModalWindowBodyProps extends IIslandProps {}
+/**
+ * Свойства компонента ModalWindowBody.
+ * `isLoading` и `loaderScreenProps` от `Island` исключены: тело скроллируется, а `LoaderScreen`
+ * позиционируется абсолютно и уезжает вместе с контентом. Состояние загрузки модального окна
+ * задаётся через одноимённые props `ModalWindowContent`.
+ */
+export interface IModalWindowBodyProps extends Omit<IIslandProps, "isLoading" | "loaderScreenProps"> {}
 
 /**
  * Тело модального окна. Обёртка над `Island` с фиксированными `type` и `size`,

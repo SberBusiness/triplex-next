9bc84a45c442912e6272ba0b7086234a27e6c402
Екатерина Табунщикова <ekaterinatabunshchikova@gmail.com>
2026-08-24 19:34:54 +0300

    TRIPLEX-872 Skeleton Переименованы типы, добавлен еще один

--- src/components/Skeleton/Skeleton.tsx
+++ src/components/Skeleton/Skeleton.tsx
@@ -5,20 +5,28 @@ import { ESkeletonType } from "./enums";
 
 export interface ISkeletonProps extends React.HTMLAttributes<HTMLDivElement> {
     /**
-     * Тип скелетона - темный или светлый.
+     * Тип скелетона. По умолчанию ESkeletonType.TYPE_2.
      */
     type?: ESkeletonType;
 }
 
+/** Соответствие типа имени класса. */
+const TYPE_TO_CLASS_NAME_MAP: Record<ESkeletonType, string> = {
+    [ESkeletonType.TYPE_1]: styles.type1,
+    [ESkeletonType.TYPE_2]: styles.type2,
+    [ESkeletonType.TYPE_3]: styles.type3,
+};
+
 /**
  * Элемент для визуализации содержимого, которое еще не загрузилось.
  */
-export const Skeleton: React.FC<ISkeletonProps> = ({ className, type = ESkeletonType.DARK, ...htmlDivAttributes }) => (
+export const Skeleton: React.FC<ISkeletonProps> = ({
+    className,
+    type = ESkeletonType.TYPE_2,
+    ...htmlDivAttributes
+}) => (
     <div
-        className={clsx(styles.skeleton, className, {
-            [styles.dark]: type === ESkeletonType.DARK,
-            [styles.light]: type === ESkeletonType.LIGHT,
-        })}
+        className={clsx(styles.skeleton, TYPE_TO_CLASS_NAME_MAP[type], className)}
         {...htmlDivAttributes}
         data-tx={process.env.npm_package_version}
     />

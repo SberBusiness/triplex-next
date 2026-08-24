e4b983119b13a8cb49a9a867daf3a51ba1b11928
Екатерина Табунщикова <ekaterinatabunshchikova@gmail.com>
2026-08-24 10:01:21 +0300

    TRIPLEX-878 Island, IslandWidget Острову добавлено свойство withourPaddings, виджет теперь задает отступы сам

--- src/components/IslandWidget/components/IslandWidgetBody.tsx
+++ src/components/IslandWidget/components/IslandWidgetBody.tsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import clsx from "clsx";
+import { IslandWidgetContext } from "../IslandWidgetContext";
+import { createSizeToClassNameMap } from "../../../utils/classNameMaps";
 import styles from "../styles/IslandWidgetBody.module.less";
 
 /** Свойства компонента IslandWidgetBody. */
 export interface IIslandWidgetBodyProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const SIZE_TO_CLASS_NAME_MAP = createSizeToClassNameMap(styles);
+
 export const IslandWidgetBody: React.FC<IIslandWidgetBodyProps> = ({ children, className, ...htmlDivAttributes }) => {
+    const { size } = useContext(IslandWidgetContext);
+
     return (
-        <div {...htmlDivAttributes} className={clsx(styles.islandWidgetBody, className)}>
+        <div {...htmlDivAttributes} className={clsx(styles.islandWidgetBody, SIZE_TO_CLASS_NAME_MAP[size], className)}>
             {children}
         </div>
     );
